24d6b4fbec85e9a00fc73787971b28069e91a15c
mjmiguel <migueljmiller@gmail.com>
2020-10-18 14:28:19 -0700

    add hooks, styles

---
+++ src/hooks/useInterval.tsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+function useInterval(callback: Function, delay: number) {
+  const savedCallback = useRef<any>();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      // node?.classList.add('something')
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
