d364e33843e434020d6f648456fdd60ecb1b940f
mjmiguel <migueljmiller@gmail.com>
2020-10-19 14:10:58 -0700

    update test scripts, config

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,8 @@
-module.exports = {
-  roots: ["<rootDir>/src"],
+import type { Config } from '@jest/types';
+import { defaults } from 'jest-config';
+
+const config: Config.InitialOptions = {
+  roots: ["<rootDir>"],
 
   // transformations for Typescript
   transform: {
@@ -17,5 +20,7 @@ module.exports = {
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
 
   // Module file extensions for importing
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+  moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"]
 };
+
+export default config;
