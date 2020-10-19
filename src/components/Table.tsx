cc1e03510f630b40ef0ea290470c036b3eafad36
mjmiguel <migueljmiller@gmail.com>
2020-10-19 09:58:17 -0700

    update TR key assignment

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -13,23 +13,24 @@ const Table: FC<TableProps> = ({ data, bookType }: TableProps) => {
 
   useEffect(() => {
     if (data) {
-      // sort best 25 first
+      // sort best 25
+      let top25;
       // sort low to high for asks
       // sort high to low for bids
-      let top25;
-      
       if (bookType === 'ask') {
         top25 = data.sort((a:exchangeObject, b:exchangeObject) => a.price - b.price ).slice(0,25);
-      } else {
+      } else if (bookType === 'bid') {
         top25 = data.sort((a:exchangeObject, b:exchangeObject) => b.price - a.price ).slice(0,25);
       }
 
-      // add best 25 to table
-      let rowArray = top25.reduce((acc, curr,) => {
-        acc.push(<TableRow key={curr.price} data={curr} />);
-          return acc;
+      if (top25) {
+        // add best 25 to table
+        let rowArray = top25.reduce((acc, curr, idx) => {
+          acc.push(<TableRow key={curr.price+curr.quantity} data={curr} />);
+            return acc;
         }, []);
         setRowArray(rowArray);
+      }
     }
   }, [data]);
 
